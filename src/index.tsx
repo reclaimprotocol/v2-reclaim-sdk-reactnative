337c904bb0128577f206ff4e74e476ac52d233a9
alimansour0002 <106339946+alimansour0002@users.noreply.github.com>
2024-01-02 03:46:31 +0300

    feat: use hook instead of class

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,17 @@
+import { useEffect, useState } from 'react';
 import { openURL, canOpenURL, parse, addEventListener } from 'expo-linking';
 import type { ProofRequest } from './interfaces';
 
-class Reclaim {
-  static async requestProof(request: ProofRequest, AppCallbackUrl: string) {
+const useReclaim = (): {
+  requestProof: (request: ProofRequest, AppCallbackUrl: string) => void;
+  deepLinkData: any;
+} => {
+  const [deepLinkData, setDeepLinkData] = useState<any>(null);
+
+  const requestProof = async (
+    request: ProofRequest,
+    AppCallbackUrl: string
+  ) => {
     const deepLink = 'reclaimprotocol://requestedproofs/';
     const deepLinkUrl = `${deepLink}?callbackUrl=${encodeURIComponent(
       AppCallbackUrl
@@ -16,31 +25,37 @@ class Reclaim {
     } else {
       console.error(`Deep linking is not supported for ${deepLink}`);
     }
+  };
 
+  useEffect(() => {
     // Handle deep link redirection
     const handleDeepLink = (event: { url: string }) => {
       // Handle the deep link URL received from the AppClip/InstantApp
       const receivedDeepLinkUrl = event.url;
 
       // Check if this is the expected deep link
-      if (receivedDeepLinkUrl.startsWith(deepLink)) {
+      if (
+        receivedDeepLinkUrl.startsWith('reclaimprotocol://requestedproofs/')
+      ) {
         // Extract parameters from the deep link URL as needed
         const params = parse(receivedDeepLinkUrl);
 
-        // Perform actions based on the received parameters
-        console.log('Deep link received:', params);
+        // Update the state with the received parameters
+        setDeepLinkData(params);
       }
     };
 
     // Add event listener for deep links
     const subscription = addEventListener('url', handleDeepLink);
 
-    // Remember to remove the event listener when your component unmounts
+    // Remember to remove the event listener when the component unmounts
     // to avoid memory leaks
     return () => {
       subscription.remove();
     };
-  }
-}
+  }, []); // Empty dependency array ensures that the effect runs once when the component mounts
 
-export default Reclaim;
+  return { requestProof, deepLinkData };
+};
+
+export default useReclaim;
