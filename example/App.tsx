20b5ac4099e3222753ad3da21247ff96ef452d85
alimansour0002 <106339946+alimansour0002@users.noreply.github.com>
2024-01-23 19:25:52 +0300

    add dev example

--- example/App.tsx
+++ example/App.tsx
@@ -5,34 +5,26 @@
  * @format
  */
 
-import {ReclaimClient} from './../src';
-
-import React from 'react';
+import React, {useEffect} from 'react';
 import type {PropsWithChildren} from 'react';
 import {
+  Pressable,
   SafeAreaView,
   ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
   useColorScheme,
   View,
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {ReclaimClient} from 'v2-reactnative-reclaim-sdk';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {Proof} from 'v2-reactnative-reclaim-sdk/lib/typescript/src/interfaces';
 
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
 const reclaimClient = new ReclaimClient('1');
-reclaimClient.applicationSecret = '1';
 
 function Section({children, title}: SectionProps): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -47,21 +39,37 @@ function Section({children, title}: SectionProps): React.JSX.Element {
         ]}>
         {title}
       </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+
+      {children}
     </View>
   );
 }
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
+  const [verificationReq, setVerificationReq] = React.useState<any>(null);
+
+  const [extracted, setExtracted] = React.useState<any>(null);
+
+  useEffect(() => {
+    const getVerificationReq = async () => {
+      reclaimClient.setAppCallbackUrl('mychat://chat/');
+      const req = await reclaimClient.createVerificationRequest([
+        'Steam ID v2',
+      ]);
+      req.on('success', (data: Proof | unknown) => {
+        if (data) {
+          const proof = data as Proof;
+          console.log('success', proof.extractedParameterValues);
+
+          setExtracted(JSON.stringify(proof.extractedParameterValues));
+        }
+      });
+      setVerificationReq(req);
+    };
+
+    getVerificationReq();
+  }, []);
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
@@ -69,32 +77,30 @@ function App(): React.JSX.Element {
 
   return (
     <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
+        <Section title="RECLAIM SDK @0.1.4 Demo" />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+          <Section title="Verify steam Username">
+            <Pressable
+              style={styles.button}
+              onPress={async () => {
+                await verificationReq.start();
+              }}>
+              <Text style={styles.sectionDescription}>Click Here!</Text>
+            </Pressable>
           </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          {extracted && (
+            <Section title="Extracted Values">
+              <Text style={styles.sectionTitle}>
+                {JSON.stringify(extracted)}
+              </Text>
+            </Section>
+          )}
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -103,20 +109,28 @@ function App(): React.JSX.Element {
 
 const styles = StyleSheet.create({
   sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+    padding: 24,
   },
   sectionTitle: {
     fontSize: 24,
     fontWeight: '600',
+    textAlign: 'center',
+    marginBottom: 16,
   },
   sectionDescription: {
-    marginTop: 8,
     fontSize: 18,
     fontWeight: '400',
+    padding: 10,
+    color: '#fff',
+    textAlign: 'center',
   },
   highlight: {
     fontWeight: '700',
+  },
+  button: {
+    fontSize: 18,
+    backgroundColor: 'gray',
+    borderRadius: 8,
   },
 });
 
